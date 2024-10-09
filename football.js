50a88f215afefcc16be46082d0ddca1fc2a89b80
adil-shafi <adil.shafi0504@gmail.com>
2024-10-09 22:41:58 +0100

    Create football.js

---
+++ football.js
@@ -0,0 +1,110 @@
+var errors = 0;
+var cardList = [
+    "haaland",
+    "kane",
+    "mbappe",
+    "messi",
+    "neymar",
+    "pickford",
+    "ronaldo",
+    "saka",
+    "salah",
+    "zinedine"
+]
+
+var cardSet;
+var board = [];
+var rows = 4;
+var columns = 5;
+
+var card1Selected;
+var card2Selected;
+
+window.onload = function() { //when page loads
+    shuffleCards();
+    startGame();
+}
+
+function shuffleCards() {
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle cards
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //gets random index
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet)
+}
+
+function startGame() {
+    //arrange board 4x5
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg);
+
+            //<img id="0-0" src="water.jpg">
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = "imagesf/" + cardImg + ".jpg";
+            card.classList.add("card");
+            card.addEventListener("click", selectCard);
+            document.getElementById("board").append(card);
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+    setTimeout(hideCards, 2000);
+}
+
+function hideCards() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "imagesf/backf.jpg";
+        }
+    }
+}
+
+
+function selectCard() {
+    if (this.src.includes("backf")) {
+        if (!card1Selected) {
+            card1Selected = this;
+
+            let coords = card1Selected.id.split("-");
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = "imagesf/" + board[r][c] + ".jpg";
+        } else if (!card2Selected && this != card1Selected) {
+            card2Selected = this;
+
+            let coords = card2Selected.id.split("-");
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = "imagesf/" + board[r][c] + ".jpg";
+            setTimeout(update, 1000);
+
+        }
+    }
+}
+
+
+function update() {
+    if (card1Selected.src != card2Selected.src) {
+        card1Selected.src = "imagesf/backf.jpg";
+        card2Selected.src = "imagesf/backf.jpg";
+        errors += 1;
+        document.getElementById("errors").innerText = errors;
+    }
+
+    card1Selected = null;
+    card2Selected = null;
+}
